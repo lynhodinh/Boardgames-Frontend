ea8a8ea0543e6301f4ceb60c41df694c3523651e
lynhodinh <lhodinh@gmail.com>
2023-04-19 12:04:49 +0100

    add conditional logic to check for 0 comments and state for loading page

--- src/Components/CommentList.jsx
+++ src/Components/CommentList.jsx
@@ -3,26 +3,41 @@ import {CommentCard} from "./CommentCard";
 import { getCommentsById } from "../api";
 
 export const CommentList = ({ review_id }) => {
-  const [commentList, setCommentList] = useState([]);
+    const [commentList, setCommentList] = useState([]);
+    const [loading, setLoading] = useState(false);
+  
 
   useEffect(() => {
+    setLoading(true)
     getCommentsById(review_id).then((comments) => {
       setCommentList(comments);
+      setLoading(false)
     });
-  });
+  }, [review_id]);
 
+  if (loading) {
+    return <p>Loading comments...</p>;
+  }
+  
   return (
     <section className="mt-8">
-      <h3 className="text-2xl font-bold mb-4">Comments:</h3>
-      <ul className="divide-y divide-gray-200">
-        {commentList.map((comment) => {
-          return (
-            <li key={comment.comment_id} className="py-4">
-              <CommentCard {...comment} />
-            </li>
-          );
-        })}
-      </ul>
+            <header>
+        <h2 className="text-2xl font-bold mb-4">Comments:</h2>
+      </header>
+      {commentList.length > 0 ? (
+        <ul className="divide-y divide-gray-200">
+          {commentList.map((comment) => {
+            return (
+              <li key={comment.comment_id}>
+                <CommentCard {...comment} />
+              </li>
+            );
+          })}
+        </ul>
+      ) : (<header>
+        <h3 className="text-gray-500">No comments yet!</h3>
+      </header>
+      )}
     </section>
   );
 };
