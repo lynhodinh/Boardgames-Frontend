ab3999fb15554eee0339b822d60534e98eb3650f
lynhodinh <lhodinh@gmail.com>
2023-04-19 09:57:42 +0100

    deleted comment count singular review

--- src/Components/SingleReview.jsx
+++ src/Components/SingleReview.jsx
@@ -20,7 +20,6 @@ export const SingleReview = () => {
           <h4 className="text-lg font-medium mb-2">Review Owner: {review.owner}</h4>
           <p className="mb-4">{review.review_body}</p>
           <p className="text-gray-600 mb-2">Votes: {review.votes}</p>
-          <p className="text-gray-600 mb-2">Comment Count: {review.comment_count}</p>
         </div>
       </section>
     );
