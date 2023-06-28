36f575fd9fd08b64152d79c2635abc0d6351ad9a
lynhodinh <lhodinh@gmail.com>
2023-06-29 00:01:37 +0100

    separate tab added for categories

--- src/Components/ReviewList.jsx
+++ src/Components/ReviewList.jsx
@@ -22,25 +22,20 @@ export const ReviewList = () => {
 
   return (
     <article className="container mx-auto py-6">
-      <aside className="fixed left-0">
-        <CategoriesNav />
-      </aside>
-      <div className="ml-48">
-        <h2 className="text-3xl font-bold mb-4 flex items-center justify-center h-full">
-          Review List
-        </h2>
+      <h2 className="text-3xl font-bold mb-4 flex items-center justify-center h-full">
+        Review List
+      </h2>
 
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {filteredReviews.map((review) => (
-            <article
-              key={review.review_id}
-              className="bg-white rounded-lg shadow-md overflow-hidden"
-            >
-              <ReviewCard {...review} />
-            </article>
-          ))}
-        </ul>
-      </div>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {filteredReviews.map((review) => (
+          <article
+            key={review.review_id}
+            className="bg-white rounded-lg shadow-md overflow-hidden"
+          >
+            <ReviewCard {...review} />
+          </article>
+        ))}
+      </ul>
     </article>
   );
 };
