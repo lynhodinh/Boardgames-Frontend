3ace040a43827d22d5398cfcbb68b6e927f47dcf
lynhodinh <lhodinh@gmail.com>
2023-06-29 00:17:03 +0100

    specify category title for every category

--- src/Components/ReviewList.jsx
+++ src/Components/ReviewList.jsx
@@ -23,7 +23,7 @@ export const ReviewList = () => {
   return (
     <article className="container mx-auto py-6">
       <h2 className="text-3xl font-bold mb-4 flex items-center justify-center h-full">
-        Review List
+        {category.charAt(0).toUpperCase() + category.slice(1)}
       </h2>
 
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
