8c12b2223b9de814dba1f064d585958e2a449ba0
lynhodinh <lhodinh@gmail.com>
2023-06-29 01:42:37 +0100

    specify categories tab title

--- src/Components/ReviewList.jsx
+++ src/Components/ReviewList.jsx
@@ -20,10 +20,14 @@ export const ReviewList = () => {
     ? reviews.filter((review) => review.category === category)
     : reviews;
 
+  const capitalizedCategory = category
+    ? category.charAt(0).toUpperCase() + category.slice(1)
+    : "All Reviews";
+
   return (
     <article className="container mx-auto py-6">
       <h2 className="text-3xl font-bold mb-4 flex items-center justify-center h-full">
-        Review List
+        {capitalizedCategory}
       </h2>
 
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
